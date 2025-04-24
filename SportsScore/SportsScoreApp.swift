6c49b405b4f0e8a1d91863becd0fccfe955cc7cb
jlambert8259 <jlambert8259@stu.d214.org>
2025-04-24 14:37:41 -0500

    Initial Commit

---
+++ SportsScore/SportsScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsScoreApp.swift
+//  SportsScore
+//
+//  Created by Jason M. Lambert on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SportsScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportsScoreTests/SportsScoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportsScoreTests.swift
+//  SportsScoreTests
+//
+//  Created by Jason M. Lambert on 4/24/25.
+//
+
+import Testing
+@testable import SportsScore
+
+struct SportsScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
